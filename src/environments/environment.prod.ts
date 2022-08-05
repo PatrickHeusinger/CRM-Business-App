64b2abbc2235d0f44290335d6dc96a5c15b69a59
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2022-08-05 12:54:52 +0200

    angular json update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyAbKZvldYMMlL-CUp7MKA2Vcvo747jkgOU",
+  authDomain: "crm-app-angular.firebaseapp.com",
+  projectId: "crm-app-angular",
+  storageBucket: "crm-app-angular.appspot.com",
+  messagingSenderId: "231873593363",
+  appId: "1:231873593363:web:f097b80d86845207d47fe6"
+  }  
 };
